ad1897e4e26587a770f816c77aa0d2a8f431f7ce
EUNJI JEON <jej.career@gmail.com>
2019-04-16 23:05:01 +0900

    all the exercises of the 1stWeek are done

---
+++ 1stWeek/workspace/progfun/.worksheet/src/week01.session3.scala
@@ -0,0 +1,28 @@
+package week01
+
+object session3 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(41); val res$0 = 
+
+	1 + 3;System.out.println("""res0: Int(4) = """ + $show(res$0));$skip(47); 
+
+  def abs(x: Double) =  if( x < 0) - x else x;System.out.println("""abs: (x: Double)Double""");$skip(338); 
+  
+	  def sqrt(x: Double) = {
+	
+		def sqrtIter(guess: Double, x:Double): Double=
+			if(isGoodEnough(guess, x)) guess
+			else sqrtIter(improve(guess, x), x)
+			
+		def isGoodEnough(guess:Double, x:Double) =
+			abs(guess * guess - x) / x < 0.001
+			
+		def improve(guess:Double, x:Double) =
+			(guess + x/ guess) / 2
+		
+		sqrtIter(1.0, x)
+	};System.out.println("""sqrt: (x: Double)Double""");$skip(11); val res$1 = 
+	
+	sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(9); val res$2 = 
+	sqrt(4);System.out.println("""res2: Double = """ + $show(res$2));$skip(13); val res$3 = 
+  sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3));$skip(13); val res$4 = 
+  sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4))}
+  }
